89d8aa71a44bb3514c003b871c041186859139fd
Priyanka2465 <154807857+Priyanka2465@users.noreply.github.com>
2024-01-08 21:53:59 +0530

    Delete Assignment work - Copy directory

--- Assignment work - Copy/Module 2(fundamentals Python)/task1.py
+++
@@ -1,9 +0,0 @@
-# Write a Python program to check if a number is positive, negative or zero.
-
-i=int(input("Enter the number="))
-if i>0:
-    print("Positive")
-elif i==0:
-    print("Zero")
-elif i<0:
-    print("Negative")
